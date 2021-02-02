1ea2d500a18afab29392de02bb3e9aefd42d623c
Guido Van Hoecke <guivho@gmail.com>
2021-02-02 21:47:36 +0100

    After creatin ApplicationType by coning Category entity
    Also adding all content from DotNetMastery site

---
+++ Rocky/Controllers/ApplicationTypeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Controllers
+{
+    public class ApplicationTypeController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ApplicationTypeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<ApplicationType> applicationTypeList = 
+                (IEnumerable<ApplicationType>)_db.ApplicationType;
+            return View(applicationTypeList);
+        }
+
+        // get - create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // post - create
+        [HttpPost]
+        public IActionResult Create(ApplicationType applicationType)
+        {
+            _db.ApplicationType.Add(applicationType);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Rocky/Controllers/CategoryController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using System.Collections.Generic;
+
+namespace Rocky.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categoryList = _db.Category;
+            return View(categoryList);
+        }
+
+        // get - create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        // post - create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            _db.Category.Add(category);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
